d41e8b2ff78385c443d857e3f85297c0871d9a6f
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-25 23:40:13 +0200

    Refactor: Improves a name

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,14 +2,14 @@
 public class FizzBuzz {
 
 	public static String fizzBuzz(int naturalNumberGreaterThanZero) {
-		if (isMultipleOfThree(naturalNumberGreaterThanZero)) {
+		if (isFizz(naturalNumberGreaterThanZero)) {
 			return "Fizz";
 		}
 		
 		return String.valueOf(naturalNumberGreaterThanZero);
 	}
 
-	private static boolean isMultipleOfThree(int naturalNumberGreaterThanZero) {
+	private static boolean isFizz(int naturalNumberGreaterThanZero) {
 		return naturalNumberGreaterThanZero % 3 == 0;
 	}
 }
