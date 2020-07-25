f269e831bf811b42fc139443f732b9f90cc82d58
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-26 01:14:18 +0200

    Introduces a class that can add wacky text depending on a given number

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,18 +1,42 @@
 import java.util.Optional;
+import java.util.function.IntPredicate;
 
 public class FizzBuzz {
 
+	private static class WackyTextGenerator {
+		private final String wackyText;
+		private final IntPredicate predicate;
+		
+		private WackyTextGenerator(String wackyText, IntPredicate predicate) {
+			this.wackyText = wackyText;
+			this.predicate = predicate;
+		}
+
+		public void addWackyText(int number, StringBuilder textOutput) {
+			if (this.predicate.test(number)) {
+				textOutput.append(this.wackyText);
+			}
+		}
+
+	}
+	
 	private static final int FIZZ_DIVISOR = 3;
 	private static final int BUZZ_DIVISOR = 5;
 
+	private static WackyTextGenerator[] WACKY_TEXT_GENERATORS = {
+			new WackyTextGenerator("Fizz", FizzBuzz::isFizz),
+			new WackyTextGenerator("Buzz", FizzBuzz::isBuzz)
+	};
+	
 	public static String fizzBuzz(int number) {
 		return generateWackyText(number).orElse(String.valueOf(number));
 	}
 
 	private static Optional<String> generateWackyText(int number) {
 		StringBuilder wackyTextBuilder = new StringBuilder();
-		fizz(number, wackyTextBuilder);
-		buzz(number, wackyTextBuilder);
+		for (WackyTextGenerator wackyTextGenerator : WACKY_TEXT_GENERATORS) {
+			wackyTextGenerator.addWackyText(number, wackyTextBuilder);
+		}
 		
 		String wackyText = wackyTextBuilder.toString();
 		if (wackyText.isEmpty()) {
