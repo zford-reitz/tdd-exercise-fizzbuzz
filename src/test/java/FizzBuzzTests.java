41f4310b3d28ccc31c3fe541e002b9fb9af32008
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-25 22:20:41 +0200

    Initializes project

---
+++ src/test/java/FizzBuzzTests.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTests {
+
+	@Test
+	void thisIsASampleFailingTest() {
+		fail("the name says it all, really");
+	}
+	
+}
