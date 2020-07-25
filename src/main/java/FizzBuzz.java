22631e56441f11acc6b54d4a636d81f47742a85e
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-07-25 23:34:41 +0200

    Refactor: Deletes obsolete code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,10 +1,6 @@
 
 public class FizzBuzz {
 
-	public static int fizzBuzz(int naturalNumberGreaterThanZero) {
-		return naturalNumberGreaterThanZero;
-	}
-
 	public static String fizzBuzzString(int naturalNumberGreaterThanZero) {
 		if (naturalNumberGreaterThanZero == 3) {
 			return "Fizz";
